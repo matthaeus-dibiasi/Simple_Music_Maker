e0446594f4edf00705b6e623efbc087d5d9adc84
Matthäus Dibiasi <matthaeus.dibiasi.1103@gmail.com>
2023-02-25 15:57:03 +0100

    Play tones and sample files (8 Bit PCM) from cli

--- Simple_Music_Maker/climain.cpp
+++ Simple_Music_Maker/climain.cpp
@@ -9,6 +9,7 @@
 
 #include <vector>
 #include <cmath>
+#include <regex>
 
 #include "clifuncs.h"
 
@@ -32,19 +33,77 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    /* Create buffer ... */
     std::vector<uint8_t> buf;
+    std::vector<uint8_t> sample_buf(BUFSIZE);
+    std::regex rgx_path("(\/[a-zA-Z0-9_]+)+[.]wav");
 
-    /* Fill buffer with arguments from cli */
+    uint8_t argument_counter = 1;
+    bool found_match;
+    int file_descriptor;
 
-    for (uint8_t tone = 0; tone < (argc - 1) / 2; tone ++) {
-        const float frequency = atof(argv[(tone + 1) * 2 - 1]);
-        const float duration = atof(argv[(tone + 1) * 2]);
+    // Fill buffer with frequencies or samples from wave files
+    for (;;) {
 
-        sine_wave_to_buffer(buf, BUFSIZE, frequency, duration);
+        /* Check if the argument is a file path */
+        std::string argument(argv[argument_counter]);
+        found_match = std::regex_match(argument, rgx_path);
+
+        // If argument is a file path ...
+        if (found_match) {
+
+            /* replace STDIN with the specified file */
+            if ((file_descriptor = open(argv[1], O_RDONLY)) < 0) {
+                fprintf(stderr, __FILE__": open() failed: %s\n", strerror(errno));
+                return 1;
+            }
+
+            if (dup2(file_descriptor, STDIN_FILENO) < 0) {
+                fprintf(stderr, __FILE__": dup2() failed: %s\n", strerror(errno));
+                return 1;
+            }
+
+            close(file_descriptor);
+
+            // Read data from wav file and load it into sample buffer
+            read(STDIN_FILENO, sample_buf.data(), sample_buf.size());
+
+            // Convert to data with only one channel
+            for (int sample = 0; sample < sample_buf.size(); sample++) {
+
+                sample_buf[sample] = round((sample_buf[sample] + sample_buf[sample + 1]) / 2); // average out the channels into one
+                sample_buf.erase(sample_buf.begin() + sample); // remove the second channel
+            }
+
+            // Allocate more memory in buffer
+            buf.resize(buf.size() + sample_buf.size());
+
+            // Move sample data to main buffer
+            for (int sample = 0; sample < sample_buf.size(); sample++) {
+
+                std::move(sample_buf.begin(), sample_buf.end(), buf.end() - sample_buf.size());
+            }
+
+            // Reallocate erased memory by erase function
+            sample_buf.resize(2 * sample_buf.size());
+
+            argument_counter++;
+        }
+
+        // If argument is a tone ...
+        else {
+
+            const float frequency = atof(argv[argument_counter]);
+            const float duration = atof(argv[argument_counter + 1]);
+
+            sine_wave_to_buffer(buf, BUFSIZE, frequency, duration);
+
+            argument_counter += 2;
+        }
+
+        if (argument_counter == argc) break;
     }
 
-    /* ... and play it */
+    /* Play the main buffer */
     if (pa_simple_write(s, buf.data(), (size_t) buf.size(), &error) < 0) {
         fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
         return 1;
